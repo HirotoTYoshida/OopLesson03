da8efcb6fe44e2d51337345abe9d67a4a8fbd83d
博人 吉田 <31158@ojs.ac.jp>
2020-10-08 11:00:36 +0900

    p177まで

---
+++ Chapter5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Chapter5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5.1
+            var a1 = Console.ReadLine();
+            var a2 = Console.ReadLine();
+            if(string.Compare(a1,a2,ignoreCase:true) == 0)
+            {
+                Console.WriteLine("等しい");
+            }
+            else
+            {
+                Console.WriteLine("等しくない");
+            }
+
+            //5.2
+            var line = Console.ReadLine();
+            int num;
+            if(int.TryParse(line, out num))
+            {
+                Console.WriteLine("#,#");
+            }
+            else
+            {
+                Console.WriteLine("数値文字列でありません");
+            }
+        }
+
+
+    }
+}
